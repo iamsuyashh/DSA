4f64002c892640970dff9e28c1c5148a2489baa3
Suyash Labde <suyashlabde123@gmail.com>
2025-07-26 10:21:01 +0530

    rotated array problems done

--- Array/checkiftheArrayisSortedorNot.cpp
+++ Array/checkiftheArrayisSortedorNot.cpp
@@ -1,27 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool isSorted(int arr[],int n){
-    for(int i = 1; i<n;i++){
-        if(arr[i]>=arr[i-1]){
-            
+bool isSorted(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= arr[i - 1])
+        {
         }else{
-             return false;
+            return false;
         }
-       
+        
     }
     return true;
 }
 
-int main(){
+int main()
+{
 
+    int arr[] = {1, 2, 3, 4, 5}, n = 5;
 
-  int arr[] = {1, 2, 3, 4, 5}, n = 5;
-
-  cout<<isSorted(arr, n);
-
-
-
+    cout << isSorted(arr, n);
 
     return 0;
 }
